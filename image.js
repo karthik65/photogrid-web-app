393e444c6bdd9af25c06067812446b761dcbb456
karthik65 <karthikboyapati3@gmail.com>
2016-10-20 17:32:12 +0530

    first commit

---
+++ image.js
@@ -0,0 +1,32 @@
+function AlbumCtrl($scope) {
+	$scope.images =[
+{ image: 'https://s-media-cache-ak0.pinimg.com/236x/88/e5/88/88e588c9d4c07aff5b01c3df08840f1d.jpg', description :'image  1 description'},
+
+{ image: 'http://thegoodproject.org/pdf/good-business-236x346.jpg', description :'image  2 description'},
+
+{ image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRXJEy9ZN_mkue5ZPc5Kt38i0w8252cMoxuNLSB20A51zezSdRD', description :'image  3 description'},
+
+{ image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQJjuY1caqJ7ed3ZDNLFtqPcNwGEMS1hoNIzRpqMcPrtk1gzG-D', description :'image  4 description'},
+
+{ image: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQK6FzXkNwESXl2jYYFZHGSX22_ZoUZvfyV8PJvUU4RUbTRKqOq', description :'image  5 description'},
+
+{ image: 'http://i.imgur.com/pPTBSbA.jpg', description :'image  6 description'},
+
+{ image: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA1qYRxQw4pdINvVQ_BQfSOgQAJZZPpKfJHgEIpOArjHST4MqR', description :'image  7 description'},
+
+{ image: 'http://independencearizona.com/wp-content/uploads/2015/01/unstoppable-236x346.jpg', description :'image  8 description'},
+
+{ image: 'http://cdn2.hubspot.net/hub/1415475/file-3438647333-jpg/blog-files/be-236x346-whatchamacallit-single.jpg', description :'image  9 description'},
+
+{ image: 'http://incognitodistribution.ca/wp-content/uploads/2016/02/18024-236x346.jpg', description :'image  10 description'}
+
+		
+
+];
+
+$scope.setcurrentImage = function(image){
+	
+	
+}
+
+}
